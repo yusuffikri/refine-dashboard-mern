e04b2a3979efc6fc4c08fed34c52cbdfba85e4df
yusuffikri <yusuffiks99@gmail.com>
2023-02-18 14:13:25 +0800

    homepage update

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,8 +1,22 @@
 import { useList } from "@pankod/refine-core";
 
+import {PieChart, PropertyReferrals, TotalRevenue, PropertyCard, TopAgent} from 'components'
+import { Typography, Box, Stack } from "@pankod/refine-mui";
+
 const home = () => {
   return (
-    <div>home</div>
+    <Box>
+      <Typography fontSize={25} fontWeight={700} color="#11142D">
+        Dashboard
+      </Typography>
+
+      <Box mt="20px" display="flex" flexWrap="wrap" gap={4}>
+      <PieChart title="Properties for sale" value={684} series={[75, 25]}
+      color={['#475be8','#e4e8ef']}/>
+    </Box>
+    </Box>
+
+    
   )
 }
 
